c50c0b716140833430b459a6c7437d7517125c63
Matthew <matthewblake0625@gmail.com>
2023-09-30 09:56:32 -0400

    passengers now match the shape of the floor.

--- The Bellhop/classes/PassengerGenerator.gd
+++ The Bellhop/classes/PassengerGenerator.gd
@@ -2,10 +2,15 @@ class_name PassengerGenerator
 extends Timer
 
 signal new_passenger
+@export var floor_textures: Array[Texture]
 
 func _ready() -> void:
 	timeout.connect(create_passenger)
 
 func create_passenger() -> void:
-	new_passenger.emit(Passenger.new())
+	var new_pass = Passenger.new()
+	if new_pass.target_floor >= 0 and new_pass.target_floor < len(floor_textures):
+		new_pass.texture = floor_textures[new_pass.target_floor] 
+
+	new_passenger.emit(new_pass)
 
